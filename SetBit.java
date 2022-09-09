fbfff1b4dc9ead0f33b002802385a2712ef3e194
Viveka Senthil <113158241+viveka-001@users.noreply.github.com>
2022-09-09 14:00:27 +0530

    Add files via upload

---
+++ SetBit.java
@@ -0,0 +1,52 @@
+
+import java.util.*;
+
+
+public class Main
+{
+	
+
+   public static void main(String[] args) {
+		
+
+       Scanner sc=new Scanner(System.in);
+		
+
+       int n=sc.nextInt();
+		
+
+       int p=sc.nextInt();
+		
+
+       if(p==0){
+		    
+
+              System.out.println("Invalid Position");
+		
+
+        }
+	    
+
+        else if((n&(1<<p-1))>0){
+		        
+
+              System.out.println("Yes");
+		   
+        
+         }
+		 
+
+         else{
+		        
+
+               System.out.println("No");
+		    
+
+          }
+		
+	
+
+       }
+
+
+  }
